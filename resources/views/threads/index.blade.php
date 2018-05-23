59e2e22a2c309adfb3cfaf647a2364fa64cf607a
Ivan Santos <ivannildosantos@hotmail.com>
2018-05-23 18:37:42 -0300

    Configurando Laravel Mix

---
+++ resources/views/threads/index.blade.php
@@ -0,0 +1,12 @@
+<meta name="csrf-token" content="{{ csrf_token()}}">
+
+<div id="app">
+    <exemple>
+        Carregando....
+    </exemple>
+</div>
+
+<script src="/js/manifest.js"></script>
+<script src="/js/vendor.js"></script>
+<script src="/js/bootstrap.js"></script>
+<script src="/js/threads.js"></script>
