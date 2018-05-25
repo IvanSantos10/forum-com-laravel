9e775bf6b995421334a54a82e5ec3e8f5e2fd1ff
Ivan Santos <ivannildosantos@hotmail.com>
2018-05-25 17:42:02 -0300

    Primeiro teste - etapa red e green

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,11 +2,13 @@
 
 namespace Tests\Feature;
 
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ExampleTest extends TestCase
 {
+    use DatabaseMigrations;
     /**
      * A basic test example.
      *
@@ -17,5 +19,19 @@ class ExampleTest extends TestCase
         $response = $this->get('/');
 
         $response->assertStatus(200);
+    }
+
+    public function testReplies()
+    {
+        $this->seed('RepliesTableSeeder');
+
+        $response = $this->get('/threads/1');
+        $response->assertStatus(200);
+
+        $response = $this->get('/threads/2');
+        $response->assertStatus(200);
+
+        $response = $this->get('/threads/a');
+        $response->assertStatus(404);
     }
 }
